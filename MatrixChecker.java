428f04fcf0b28ef8c8ca8c229d992cb472bf6e69
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2014-10-25 23:46:24 +0100

    initial commit

---
+++ MatrixChecker.java
@@ -0,0 +1,14 @@
+public MatrixChecker {
+
+	public boolean isSymmetrical(int[] valueSet) {
+
+	}
+
+	public boolean isSymmetrical(int[][] valueSet) {
+
+	}
+
+	public boolean isTriangular(int[][] valueSet) {
+
+	}
+}
