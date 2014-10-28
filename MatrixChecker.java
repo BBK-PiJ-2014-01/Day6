d54435ebd308a55d0a8b5dadc3610f7b4c3c6b55
unknown <pmeyer02@mail.bbk.ac.uk>
2014-10-28 23:22:44 +0000

    methods coded but not tested yet

--- MatrixChecker.java
+++ MatrixChecker.java
@@ -1,14 +1,31 @@
 public MatrixChecker {
 
 	public boolean isSymmetrical(int[] valueSet) {
-
+		boolean result = true;
+		int i = 0;
+		do {
+			if (valueSet[i] != valueSet[valueSet.length-1-i])
+				result = false;
+			i++;
+		} while ((result == true) && (i <= valueSet.length-1-i))
+		return(result);
 	}
 
 	public boolean isSymmetrical(int[][] valueSet) {
-
+		boolean result = true;
+		for (int i=0; i<valueSet.length; i++)
+			for (int j=0; j<valueSet.length;j++)
+				if (valueSet[i][j] != valueSet[j][i])
+					result = false;
+		return(result);
 	}
 
 	public boolean isTriangular(int[][] valueSet) {
-
+		boolean result = true;
+		for (int i=0; i<valueSet.length; i++)
+			for (int j=0; j<valueSet.length;j++)
+				if ((i>j) && (valueSet[i][j] != 0))
+					result = false;
+		return(result);
 	}
 }
