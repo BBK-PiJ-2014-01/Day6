af6d4948343002f148927c2d03ca359d2c8b9236
unknown <pmeyer02@mail.bbk.ac.uk>
2014-10-28 00:12:56 +0000

    code complete

--- Matrix.java
+++ Matrix.java
@@ -18,45 +18,69 @@ public class Matrix {
 	}
 
 	public boolean setRow (int row, String valueSet) {
-		String[] tempSet = new String[matrix2D.length];
+		String[] tempSet = new String[matrix2D[0].length];
+		for (int i=0; i<tempSet.length; i++)
+			tempSet[i]="";
 		if ((row<=matrix2D.length)) {
 			int j = 0;	
 			for (int i=0; i<valueSet.length(); i++) {
-				if (Character.isDigit(valueSet.charAt(i))) {
+				if (Character.isDigit(valueSet.charAt(i)))
 					tempSet[j] += valueSet.charAt(i);
-					System.out.println(valueSet.charAt(i));
-				}
 				else
-					if (valueSet.charAt(i)==',') {
-						System.out.println(tempSet[j]);
+					if (valueSet.charAt(i)==',')
 						j++;
-					}
 					else
-						return(false);
+						return(false);			
 			}
-			System.out.println(j);
-			System.out.println(matrix2D[0].length);
 			if (j==matrix2D[0].length-1) {
-				for (int i=0; i<j; i++) {
+				for (int i=0; i<=j; i++)
 					matrix2D[row][i] = Integer.parseInt(tempSet[i]);
-//					System.out.println(Integer.parseInt(tempSet[i]));
-				}
 				return(true);
 			} else
 				return(false);
 		} else
 			return(false);
 	}
-/*
-	public boolean setColumn (int column, String valueSet) {
 
+	public boolean setColumn (int column, String valueSet) {
+		String[] tempSet = new String[matrix2D.length];
+		for (int i=0; i<tempSet.length; i++)
+			tempSet[i]="";
+		if ((column<=matrix2D[0].length)) {
+			int j = 0;	
+			for (int i=0; i<valueSet.length(); i++) {
+				if (Character.isDigit(valueSet.charAt(i)))
+					tempSet[j] += valueSet.charAt(i);
+				else
+					if (valueSet.charAt(i)==',')
+						j++;
+					else
+						return(false);			
+			}
+			if (j==matrix2D.length-1) {
+				for (int i=0; i<=j; i++)
+					matrix2D[i][column] = Integer.parseInt(tempSet[i]);
+				return(true);
+			} else
+				return(false);
+		} else
+			return(false);
 	}
 
 	public String toString() {
 		String output = "[";
-
+		for (int i=0; i< matrix2D.length; i++)
+			for (int j=0; j< matrix2D[0].length; j++) {
+				output = output + matrix2D[i][j];
+				if (j==matrix2D[0].length-1)
+					output += ";";
+				else
+					output += ",";
+			}
+		output += "]";
+		return(output);	
 	}
-*/
+
 	public void prettyPrint() {
 		for (int i=0; i<matrix2D.length ; i++) {
 			System.out.println();
